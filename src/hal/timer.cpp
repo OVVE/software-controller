38646a5bf7782c02b5e5d6e9067bc30487e0327d
Rosen, Michael R <michael.r.rosen@intel.com>
2020-03-30 19:25:04 -0700

    hal: add timer api WIP
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

---
+++ src/hal/timer.cpp
@@ -0,0 +1,21 @@
+
+#include "../hal/hal.h"
+#include "../hal/timer.h"
+
+int timerHalInit(void)
+{
+  // TODO: Implement
+  return HAL_OK;
+}
+
+int timerHalBegin(struct timer* timer, unsigned int duration)
+{
+  // TODO: Implement
+  return HAL_OK;
+}
+
+int timerHalRun(struct timer* timer)
+{
+  // TODO: Implement
+  return HAL_OK;
+}
