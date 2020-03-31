e086d53611c7833ba737f8b9e39ceba999740d80
Mcinnis <russ.mcinnis@intel.com>
2020-03-31 09:53:03 -0700

    added serial.cpp and serial.h

---
+++ src/hal/serial.h
@@ -0,0 +1,16 @@
+
+#ifndef __SERIAL_HAL_H__
+#define __SERIAL_HAL_H__
+
+#include "../hal/hal.h"
+
+// TODO: Doc
+int serialHalInit(void);
+
+// TODO: Doc
+int serialHalGetData(void);
+
+// TODO: Doc
+int serialHalSendData();
+
+#endif /* __SERIAL_HAL_H__ */
