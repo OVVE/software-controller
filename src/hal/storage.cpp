7cd2a5a57b155b2d3ae1e97efadfc0411aca5373
Rosen, Michael R <michael.r.rosen@intel.com>
2020-03-30 19:26:30 -0700

    hal: add storage hal WIP
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

---
+++ src/hal/storage.cpp
@@ -0,0 +1,21 @@
+
+#include "../hal/hal.h"
+#include "../hal/storage.h"
+
+int storageHalInit(void)
+{
+  // TODO: Implement
+  return HAL_OK;
+}
+
+int storageHalRead(unsigned int address, void* buffer, unsigned int size)
+{
+  // TODO: Implement
+  return HAL_OK;
+}
+
+int storageHalWrite(unsigned int address, void* buffer, unsigned int size)
+{
+  // TODO: Implement
+  return HAL_OK;
+}
