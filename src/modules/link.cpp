f5f2c5830495ce0caaf3f5cb39c6f3b7f0917d89
Mcinnis <russ.mcinnis@intel.com>
2020-03-31 09:53:43 -0700

    update link.cpp with some of the serial parts

--- src/modules/link.cpp
+++ src/modules/link.cpp
@@ -1,16 +1,76 @@
 //
 // Link Module
 //
-
+#include "../pt/pt.h"
 #include "../modules/link.h"
 #include "../modules/module.h"
+#include "../hal/serial.h"
 
-// Public Variables
-// TODO: Fix this variable name :(
-struct link comm;
+// Private Variables
+static struct pt serialThread;
+static struct pt serialReadThread;
+static struct pt serialSendThread;
+
+static PT_THREAD(serialReadThreadMain(struct pt* pt))
+{
+  PT_BEGIN(pt);
+
+  PT_WAIT_UNTIL(pt, serialHalGetData() != HAL_IN_PROGRESS);
+
+  //int rawPressure;
+  //pressureSensorHalGetValue(&rawPressure);
+  
+  // TODO: Process pressure reading, store in public variable
+  //currentPressure = 0;
+  //peakPressure = 0;
+  //plateauPressure = 0;
+  
+  // TODO: add sampling rate delay
+
+  PT_RESTART(pt);
+  PT_END(pt);
+}
+
+static PT_THREAD(serialSendThreadMain(struct pt* pt))
+{
+  PT_BEGIN(pt);
+
+  PT_WAIT_UNTIL(pt, serialHalSendData() != HAL_IN_PROGRESS);
+
+  //int rawPressure;
+  //pressureSensorHalGetValue(&rawPressure);
+  
+  // TODO: Process pressure reading, store in public variable
+  //currentPressure = 0;
+  //peakPressure = 0;
+  //plateauPressure = 0;
+  
+  // TODO: add sampling rate delay
+
+  PT_RESTART(pt);
+  PT_END(pt);
+}
+
+PT_THREAD(serialThreadMain(struct pt* pt))
+{
+  PT_BEGIN(pt);
+
+  if (!PT_SCHEDULE(serialReadThreadMain(&serialReadThread))) {
+    PT_EXIT(pt);
+  }
+
+  if (!PT_SCHEDULE(serialSendThreadMain(&serialSendThread))) {
+    PT_EXIT(pt);
+  }
+  PT_RESTART(pt);
+  PT_END(pt);
+}
 
 int linkModuleInit(void)
 {
+  if (serialHalInit() != HAL_OK) {
+    return MODULE_FAIL;
+  }
   // TODO: Implement
   return MODULE_OK;
 }
@@ -18,5 +78,5 @@ int linkModuleInit(void)
 int linkModuleRun(void)
 {
   // TODO: Implement
-  return MODULE_OK;
+  return (PT_SCHEDULE(serialThreadMain(&serialThread))) ? MODULE_OK : MODULE_FAIL;
 }
