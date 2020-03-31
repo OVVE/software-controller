2672c6076cbff2ff6d9fee2a0719394204a286dd
Rosen, Michael R <michael.r.rosen@intel.com>
2020-03-30 19:25:04 -0700

    hal: add timer api WIP
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

--- src/hal/timer.h
+++ src/hal/timer.h
@@ -4,13 +4,23 @@
 
 #include "../hal/hal.h"
 
+// Time units
+#define SEC   * 1
+#define MSEC  * 1000
+#define USEC  * 1000000
+
+struct timer {
+  unsigned int start;
+  unsigned int duration;
+};
+
 // TODO: Doc
 int timerHalInit(void);
 
 // TODO: Doc
-int timerHalBegin(unsigned int duration);
+int timerHalBegin(struct timer* timer, unsigned int duration);
 
 // TODO: Doc
-int timerHalRun(void);
+int timerHalRun(struct timer* timer);
 
 #endif /* __TIMER_HAL_H__ */
