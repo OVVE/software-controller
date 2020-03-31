0f1574ca9bec45510da1093dd7d9fb1517a11f00
Rosen, Michael R <michael.r.rosen@intel.com>
2020-03-30 19:26:54 -0700

    modules: implement more module code WIP
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

--- src/modules/control.cpp
+++ src/modules/control.cpp
@@ -5,15 +5,18 @@
 #include "../pt/pt.h"
 
 #include "../hal/motor.h"
+#include "../hal/timer.h"
 
 #include "../modules/module.h"
 #include "../modules/control.h"
+#include "../modules/parameters.h"
 
 // Public Variables
 int controlState = CONTROL_IDLE;
 
 // Private Variables
 static struct pt controlThread;
+static struct timer controlTimer;
 
 static PT_THREAD(controlThreadMain(struct pt* pt))
 {
@@ -21,20 +24,53 @@ static PT_THREAD(controlThreadMain(struct pt* pt))
   
   while (1) {
     if (controlState == CONTROL_IDLE) {
-      // TODO: Logic to move out of idle
+      // Wait for the parameters to enter the run state before
+      if (parameters.run) {
+        controlState = CONTROL_BEGIN_INHALATION;
+      }
+      
     } else if (controlState == CONTROL_BEGIN_INHALATION) {
-      // TODO: Calculate motor
-      unsigned int motorCompressionDistance = 0;
-      unsigned int motorCompressionDuration = 0;
-      motorHalBegin(MOTOR_HAL_DIRECTION_INHALATION, motorCompressionDistance, motorCompressionDuration);
+      if (!parameters.assist) {
+        // TODO: Calculate motor
+        unsigned int motorCompressionDistance = 0;
+        unsigned int motorCompressionDuration = 0;
+        // TODO: Error check
+        motorHalBegin(MOTOR_HAL_DIRECTION_INHALATION, motorCompressionDistance, motorCompressionDuration);
+      } else {
+        // TODO: Implement Assist mode setup
+      }
       controlState = CONTROL_INHALATION;
+      
     } else if (controlState == CONTROL_INHALATION) {
-      PT_WAIT_UNTIL(pt, motorHalRun() != HAL_IN_PROGRESS);
+      if (!parameters.assist) {
+        PT_WAIT_UNTIL(pt, motorHalRun() != HAL_IN_PROGRESS);
+      } else {
+        // TODO: Implement Assist mode run
+      }
       controlState = CONTROL_BEGIN_HOLD_IN;
+      
     } else if (controlState == CONTROL_BEGIN_HOLD_IN) {
-      // TODO: Implement begin hold state
+      timerHalBegin(&controlTimer, 150 MSEC);
+      controlState = CONTROL_HOLD_IN;
+      
+    } else if (controlState == CONTROL_HOLD_IN) {
+      PT_WAIT_UNTIL(pt, timerHalRun(&controlTimer) != HAL_IN_PROGRESS);
+      controlState = CONTROL_BEGIN_EXHALATION;
+      
+    } else if (controlState == CONTROL_BEGIN_EXHALATION) {
+      unsigned int motorDecompressionDistance = 0;
+      unsigned int motorDecompressionDuration = 0;
+      motorHalBegin(MOTOR_HAL_DIRECTION_EXHALATION, motorDecompressionDistance, motorDecompressionDuration);
+      controlState = CONTROL_EXHALATION;
+      
+    } else if (controlState == CONTROL_EXHALATION) {
+      PT_WAIT_UNTIL(pt, motorHalRun() != HAL_IN_PROGRESS);
+      controlState = (parameters.run) ? CONTROL_BEGIN_INHALATION : CONTROL_IDLE;
+      
+    } else {
+      // TODO: Error, unknown control state!!!
+      controlState = CONTROL_IDLE;
     }
-    // TODO: Implement rest of control FSM
   }
   
   // Should never reach here
@@ -46,6 +82,9 @@ int controlModuleInit(void)
   if (motorHalInit() != HAL_OK) {
     return MODULE_FAIL;
   }
+  
+  PT_INIT(&controlThread);
+  
   return MODULE_OK;
 }
 
--- src/modules/link.h
+++ src/modules/link.h
@@ -2,6 +2,19 @@
 #ifndef __LINK_MODULE_H__
 #define __LINK_MODULE_H__
 
+#include <stdbool.h>
+
+struct link {
+  bool update;
+  bool assist;
+  unsigned int volumeRequested;
+  unsigned int respirationRateRequested;
+  unsigned int ieRatioRequested;
+};
+
+// Public Variables
+extern struct link comm;
+
 // TODO: Doc
 int linkModuleInit(void);
 
