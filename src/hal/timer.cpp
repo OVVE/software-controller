d07ed5a215b99df7c32b700fff5d6d3cf64726c4
Rosen, Michael R <michael.r.rosen@intel.com>
2020-03-31 13:58:52 -0700

    timer: implement timer hal with Arduino libraries WIP
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

--- src/hal/timer.cpp
+++ src/hal/timer.cpp
@@ -1,21 +1,33 @@
 
+#include <Arduino.h>
+
 #include "../hal/hal.h"
 #include "../hal/timer.h"
 
 int timerHalInit(void)
 {
-  // TODO: Implement
+  // No initialized needed in this case
   return HAL_OK;
 }
 
 int timerHalBegin(struct timer* timer, unsigned int duration)
 {
-  // TODO: Implement
-  return HAL_OK;
+  if (timer) {
+    timer->start = (unsigned int) micros();
+    timer->duration = duration;
+    return HAL_OK;
+  }
+  return HAL_FAIL;
 }
 
 int timerHalRun(struct timer* timer)
 {
-  // TODO: Implement
-  return HAL_OK;
+  if (timer) {
+    if ((unsigned int) micros() - timer->start < timer->duration) {
+      return HAL_IN_PROGRESS;
+    } else {
+      return HAL_TIMEOUT;
+    }
+  }
+  return HAL_FAIL;
 }
