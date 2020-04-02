3bc68493bbd5d48cda29cb8987b40eb921a39bcc
Mcinnis <russ.mcinnis@intel.com>
2020-04-02 11:54:40 -0700

    update serial with ccitt crc16 check

--- src/hal/serial.cpp
+++ src/hal/serial.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>
-#include <crc16.h>
 
 #include "../pt/pt.h"
 #include "../hal/hal.h"
 #include "../hal/serial.h"
 
 #define MaxLength 200
-
-byte BufferLength;
-String Buffer;
-char SendBuffer[MaxLength + 1];
 
 uint32_t last_send_ms = 0;  // this is used for send interval
 uint32_t current_send_ms = 0;
@@ -57,7 +52,7 @@ data_packet_def data_packet;
 data_packet_def data_packet_last_sent;
 
 //command_packet_def command_packet;
-
+// this is used to handle byte by byte input
 union incoming_packet_u {
   command_packet_def command_packet;
   uint8_t command_bytes[sizeof(command_packet)];
@@ -67,12 +62,54 @@ union incoming_packet_u {
 // data for handling serial reads
 incoming_packet_u incoming_packet;
 int incoming_index = 0;
-incoming_packet_u shared_command_packet; // this should be an extern in other modules that need to read this
-incoming_packet_u command_packet_reference; // this is saved in case other modules overwrite the shared copy
+//incoming_packet_u shared_command_packet; // this should be an extern in other modules that need to read this
+//incoming_packet_u command_packet_reference; // this is saved in case other modules overwrite the shared copy
+command_packet_def public_command_packet;
+command_packet_def command_packet_reference;
+
+#define POLY 0x8408
+/*
+//                                      16   12   5
+// this is the CCITT CRC 16 polynomial X  + X  + X  + 1.
+// This works out to be 0x1021, but the way the algorithm works
+// lets us use 0x8408 (the reverse of the bit pattern).  The high
+// bit is always assumed to be set, thus we only use 16 bits to
+// represent the 17 bit value.
+*/
+uint8_t crc_i;
+uint16_t crc_data;
+uint16_t crc = 0xffff;
+
+uint16_t crc16(uint8_t *data_p, uint16_t length)
+{
+      //unsigned char i;
+      //unsigned int data;
+      //unsigned int crc = 0xffff;
+
+      if (length == 0)
+            return (~crc);
+
+      do
+      {
+            for (crc_i=0, crc_data=(uint16_t)0xff & *data_p++;
+                 crc_i < 8; 
+                 crc_i++, crc_data >>= 1)
+            {
+                  if ((crc_i & 0x0001) ^ (crc_data & 0x0001))
+                        crc = (crc >> 1) ^ POLY;
+                  else  crc >>= 1;
+            }
+      } while (--length);
+
+      crc = ~crc;
+      crc_data = crc;
+      crc = (crc << 8) | (crc_data >> 8 & 0xff);
+
+      return (crc);
+}
 
 int serialHalInit(void)
 {
-  SendBuffer[0] = NULL;
   Serial.begin(115200);  // this function does not return anything
   // not sure how we can do something like while (!serial)
   // return MODULE_FAIL;
@@ -84,13 +121,38 @@ int serialHalGetData(void)
   while (Serial.available())
   {       
     inByte = Serial.read();
+    
+    // verify that the command is responding to the correct data packet by checking the sequence number and rev
+    if (incoming_index == 0)
+    {
+      if (incoming_packet.command_packet.sequence_count != data_packet_last_sent.sequence_count)
+      {
+        // set alarm bit
+        
+        // do not increment incoming_index and return
+        return (HAL_OK);
+      }
+    }
+
+    if (incoming_index == 1)
+    {
+      if (incoming_packet.command_packet.packet_version != data_packet_last_sent.packet_version)
+      {
+        // set alarm bit
+        
+        // reset incoming_index and return
+        incoming_index = 0;
+        return (HAL_OK);
+        
+      }      
+    }
     incoming_packet.command_bytes[incoming_index] = inByte;
     incoming_index++;
     if (incoming_index >= sizeof(incoming_packet.command_bytes))
     {
       // save a copy for other modules, but keep a reference in case the shared copy gets modified
-      memcpy(shared_command_packet.command_bytes, incoming_packet.command_bytes, sizeof(incoming_packet.command_bytes));
-      memcpy(command_packet_reference.command_bytes, incoming_packet.command_bytes, sizeof(incoming_packet.command_bytes));
+      memcpy(public_command_packet, incoming_packet.command_packet, sizeof(public_command_packet));
+      memcpy(command_packet_reference, incoming_packet.command_packet, sizeof(command_packet_reference));
       incoming_index = 0;
       return HAL_OK;
     } 
@@ -103,7 +165,9 @@ int serialHalSendData()
   current_send_ms = millis();
   if ((current_send_ms - last_send_ms) >= send_interval_ms)
   {
-    data_packet.packet_version = '1';
+    //data_packet.packet_version = '1';
+    //unsigned short crc16(char *data_p, unsigned short length) 
+    data_packet.crc = crc16((uint8_t *)&data_packet, sizeof(data_packet) - 2);
     memcpy((void *)&data_packet_last_sent, (void *)&data_packet, sizeof(data_packet_last_sent));
     bytesSent = Serial.write((byte *)&data_packet, sizeof(data_packet));
     if (bytesSent != sizeof(data_packet)) {
@@ -113,3 +177,4 @@ int serialHalSendData()
   }
   return HAL_OK;
 }
+
