0d5523069bab3a0dec31c43472e50dee389d387a
Noel Paz <noel.f.paz@intel.com>
2020-04-03 05:22:10 -0600

    TestReader modified solved issue with timing

--- ecuculink/ui_ecu_link.py
+++ ecuculink/ui_ecu_link.py
@@ -1,6 +1,10 @@
 import serial
 import sys
+from time import sleep
 ser = 0
+BAUD = 115200
+PORT = "/dev/ttyACM0"
+SER_TIMEOUT = .9
 
 class vent_monitor():
 
@@ -13,7 +17,52 @@ class vent_monitor():
         setting_curr = init_setting
         setting_new = None
         prev_seq_no = None
+        init_serial()
 
+    def process_in_serial():
+        global ser
+        in_pkt={'sequence_count': 0,            # bytes 0 - 1 - rpi unsigned short
+                'packet_version': 0,             # byte 2      - rpi unsigned char
+                'mode_value': 0,                 # byte 3      - rpi unsigned char
+                'respiratory_rate_measured': 0, # bytes 4 - 7 - rpi unsigned int
+                'respiratory_rate_set': 0,      # bytes 8 - 11
+                'tidal_volume_measured': 0,     # bytes 12 - 15
+                'tidal_volume_set': 0,          # bytes 16 - 19
+                'ie_ratio_measured': 0,         # bytes 20 - 23
+                'ie_ratio_set': 0,              # bytes 24 - 27
+                'peep_value_measured': 0,       # bytes 28 - 31
+                'peak_pressure_measured': 0,    # bytes 32 - 35
+                'plateau_value_measurement': 0, # bytes 36 - 39
+                'pressure_measured': 0,         # bytes 40 - 43
+                'flow_measured': 0,             # bytes 44 - 47
+                'volume_in_measured': 0,        # bytes 48 - 51
+                'volume_out_measured': 0,       # bytes 52 - 55
+                'volume_rate_measured': 0,      # bytes 56 - 59
+                'control_state': 0,              # byte 60       - rpi unsigned ch
+                'battery_level': 0,              # byte 61
+                'reserved': 0,                  # bytes 62 - 63 - rpi unsigned int
+                'alarm_bits': 0,                # bytes 64 - 67
+                'crc': 0 } 
+        while 1:
+            byteData = read_all(ser, 70)
+            #byteData = ser.read(70)
+            #2 ways to print
+            #print (byteData)  #raw will show ascii if can be decoded
+            #hex only -- byte order is reversed
+            #print(''.join(r'\x'+hex(letter)[2:] for letter in byteData))
+            # Workaround
+            try:
+                in_pkt['ver']=byteData[4]
+            except:
+                pass
+            finally:
+                pass
+            #
+            in_pkt['seq']=int.from_bytes(byteData[0:2], byteorder='little')
+            end = None
+            in_pkt['crc']=int.from_bytes(byteData[72:None], byteorder='little')
+            in_pkt['mode']=byteData[3]
+            print (in_pkt)
 
     def recv_arduino_packet(self, arduino_packet):
         """
@@ -147,15 +196,16 @@ class vent_monitor():
     
     global ser          #Must be declared in Each Function
     ser = serial.Serial()
-    ser.baudrate = 115200
-    ser.port = "/dev/ttyACM0" 
+    ser.baudrate = BAUD
+    ser.port = PORT 
     #a little less than polling speed from arduino
-    ser.timeout = 0.9
+    ser.timeout = SER_TIMEOUT
     ser.open()          #Opens SerialPort
 
     # print port open or closed
     if ser.isOpen():
         print ('Open: ' + ser.portstr)
+    sleep(1)
     #Function Ends Here
         
 
