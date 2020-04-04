12529b6ce347914d81df6f30ef73254106fc2719
Mcinnis <russ.mcinnis@intel.com>
2020-04-03 19:27:56 -0700

    added simple checksum and setting crc field to the checksum value

--- src/modules/link.cpp
+++ src/modules/link.cpp
@@ -23,45 +23,18 @@ command_packet_def command_packet_reference;
 command_packet_def command_packet;
 bool ready_to_send = true;
 
-#define POLY 0x8408
-/*
-//                                      16   12   5
-// this is the CCITT CRC 16 polynomial X  + X  + X  + 1.
-// This works out to be 0x1021, but the way the algorithm works
-// lets us use 0x8408 (the reverse of the bit pattern).  The high
-// bit is always assumed to be set, thus we only use 16 bits to
-// represent the 17 bit value.
-*/
-uint8_t crc_i;
-uint32_t crc_data;
-uint32_t crc = 0xffff;
 
-uint16_t crc16(int8_t *data_p, uint16_t length)
+int16_t sum_return;
+int16_t sum_index;
+
+int16_t sum(uint8_t datap[], uint16_t data_length)
 {
-      //unsigned char i;
-      //unsigned int data;
-      //unsigned int crc = 0xffff;
-
-      if (length == 0)
-            return (~crc);
-
-      do
-      {
-            for (crc_i=0, crc_data=(unsigned int)0xff & *data_p++;
-                 crc_i < 8; 
-                 crc_i++, crc_data >>= 1)
-            {
-                  if ((crc_i & 0x0001) ^ (crc_data & 0x0001))
-                        crc = (crc >> 1) ^ POLY;
-                  else  crc >>= 1;
-            }
-      } while (--length);
-
-      crc = ~crc;
-      crc_data = crc;
-      crc = (crc << 8) | (crc_data >> 8 & 0xff);
-
-      return (crc);
+  sum_return = 0;
+  for (sum_index = 0; sum_index < data_length; sum_index++)
+  {
+    sum_return += datap[sum_index];
+  }
+  return (sum_return);
 }
 
 static PT_THREAD(serialReadThreadMain(struct pt* pt))
@@ -101,7 +74,7 @@ static PT_THREAD(serialSendThreadMain(struct pt* pt))
     public_data_packet.start_byte = 0xFF;
     public_data_packet.sequence_count = sequence_count;
     public_data_packet.packet_version = PACKET_VERSION;
-    public_data_packet.crc = crc16((int8_t *)&public_data_packet, sizeof(public_data_packet) - 2);
+    public_data_packet.crc = ((int8_t *)&public_data_packet, sizeof(public_data_packet) - 2);
     //memcpy((void *)&data_packet_last_sent, (void *)&public_data_packet, sizeof(data_packet_last_sent));
     //public_data_packet.crc = crc16((unsigned int *)&public_data_packet, sizeof(public_data_packet) - 2);    
   }    
