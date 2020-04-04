f21e1d3d1352ac82b4c2b541ea385f0bdeb89740
Mcinnis <russ.mcinnis@intel.com>
2020-04-04 11:58:31 -0700

    put data structs in comms

--- src/modules/link.cpp
+++ src/modules/link.cpp
@@ -13,13 +13,9 @@ static struct pt serialThread;
 static struct pt serialReadThread;
 static struct pt serialSendThread;
 
-command_packet_def public_command_packet;
-data_packet_def public_data_packet;
 //data_packet_def data_packet_last_sent;
 
 //incoming_packet_u shared_command_packet; // this should be an extern in other modules that need to read this
-//incoming_packet_u command_packet_reference; // this is saved in case other modules overwrite the shared copy
-command_packet_def command_packet_reference;
 command_packet_def command_packet;
 bool ready_to_send = true;
 
@@ -42,26 +38,23 @@ static PT_THREAD(serialReadThreadMain(struct pt* pt))
   PT_BEGIN(pt);
 
   PT_WAIT_UNTIL(pt, serialHalGetData() != HAL_IN_PROGRESS);
-  // save a copy for other modules, but keep a reference in case the shared copy gets modified
-  //memcpy((void *)&public_command_packet, (void *)&incoming_packet.command_packet, sizeof(public_command_packet));
-  //memcpy((void *)&command_packet_reference, (void *)&incoming_packet.command_packet, sizeof(command_packet_reference));
+
   if (watchdog_exceeded == true)
   {
-    public_command_packet.alarm_bits |= 1 << ALARM_DROPPED_PACKET;
+    comm.public_command_packet.alarm_bits |= 1 << ALARM_DROPPED_PACKET;
     watchdog_exceeded = false;
     clear_input = true;
     ready_to_send = true;
   }
   //pcommand_packet = (unsigned char *)&command_packet;
-  else if ((public_command_packet.sequence_count != last_sequence_count) || (public_command_packet.start_byte != 0xFF))
+  else if ((comm.public_command_packet.sequence_count != last_sequence_count) || (comm.public_command_packet.start_byte != 0xFF))
   {
-    public_command_packet.alarm_bits |= 1 << ALARM_CRC_ERROR;
+    comm.public_command_packet.alarm_bits |= 1 << ALARM_CRC_ERROR;
   }
-  else if (public_command_packet.crc != sum((uint8_t *)&public_command_packet, sizeof(public_command_packet) - 2))
+  else if (comm.public_command_packet.crc != sum((uint8_t *)&comm.public_command_packet, sizeof(comm.public_command_packet) - 2))
   {
     //public_command_packet.alarm_bits |= 1 << ALARM_CRC_ERROR;
   }    
-  //memcpy((void *)&command_packet_reference, (void *)pcommand_packet, sizeof(command_packet_reference)); 
   
   ready_to_send = true;
   PT_RESTART(pt);
@@ -75,12 +68,10 @@ static PT_THREAD(serialSendThreadMain(struct pt* pt))
   PT_WAIT_UNTIL(pt, serialHalSendData() != HAL_IN_PROGRESS);
 
   if (sequence_count != last_sequence_count) {
-    public_data_packet.start_byte = 0xFF;
-    public_data_packet.sequence_count = sequence_count;
-    public_data_packet.packet_version = PACKET_VERSION;
-    public_data_packet.crc = sum((uint8_t *)&public_data_packet, sizeof(public_data_packet) - 2);
-    //memcpy((void *)&data_packet_last_sent, (void *)&public_data_packet, sizeof(data_packet_last_sent));
-    //public_data_packet.crc = crc16((unsigned int *)&public_data_packet, sizeof(public_data_packet) - 2);    
+    comm.public_data_packet.start_byte = 0xFF;
+    comm.public_data_packet.sequence_count = sequence_count;
+    comm.public_data_packet.packet_version = PACKET_VERSION;
+    comm.public_data_packet.crc = sum((uint8_t *)&comm.public_data_packet, sizeof(comm.public_data_packet) - 2);
   }    
   PT_RESTART(pt);
   PT_END(pt);
