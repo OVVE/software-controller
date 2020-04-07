ccc53cdfff56645a2a6ab36b1aea1416d9bd4821
Rosen, Michael R <michael.r.rosen@intel.com>
2020-04-06 18:00:46 -0700

    timer: fix bugs and expand functionality
    . Refactor timer to use uint32_t
    . Fix incorrect unit macros
    . Add timerHalCurrent to get the elasped time on the timer
    
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

--- src/hal/timer.h
+++ src/hal/timer.h
@@ -2,25 +2,30 @@
 #ifndef __TIMER_HAL_H__
 #define __TIMER_HAL_H__
 
+#include <stdint.h>
+
 #include "../hal/hal.h"
 
 // Time units
-#define SEC   * 1
+#define SEC   * 1000000
 #define MSEC  * 1000
-#define USEC  * 1000000
+#define USEC  * 1
 
 struct timer {
-  unsigned int start;
-  unsigned int duration;
+  uint32_t start;
+  uint32_t duration;
 };
 
 // TODO: Doc
 int timerHalInit(void);
 
 // TODO: Doc
-int timerHalBegin(struct timer* timer, unsigned int duration);
+int timerHalBegin(struct timer* timer, uint32_t duration);
 
 // TODO: Doc
 int timerHalRun(struct timer* timer);
+
+// TODO: Doc
+uint32_t timerHalCurrent(struct timer* timer);
 
 #endif /* __TIMER_HAL_H__ */
