c930adacf392419c4e40b4b3675fd1e2e515087b
Mcinnis <russ.mcinnis@intel.com>
2020-04-07 19:47:57 -0700

    use temporary storge for command data so it checks out before copying to public. also do not write alarm bits.

--- src/modules/link.cpp
+++ src/modules/link.cpp
@@ -7,6 +7,7 @@
 #include "../modules/link.h"
 #include "../modules/module.h"
 #include "../hal/serial.h"
+#include <Arduino.h>
 
 FastCRC16 CRC16;  // this is the class to create crc16 to match the CCITT crc16 that rpi is using
 
@@ -21,6 +22,7 @@ uint16_t calc_crc;
 // copy the public data packet to this buffer because there might be changes
 // by other modules while serial.cpp is waiting to send this
 data_packet_def update_crc_data_packet;
+command_packet_def command_packet_from_serial;
 
 #ifdef SERIAL_DEBUG
 extern HardwareSerial &serial_debug;
@@ -28,6 +30,8 @@ extern HardwareSerial &serial_debug;
 uint32_t watchdog_count = 0;
 uint32_t dropped_packet_count = 0;
 uint32_t packet_count = 0;
+
+uint16_t debug_index;
 
 static PT_THREAD(serialReadThreadMain(struct pt* pt))
 {
@@ -42,55 +46,62 @@ if ((packet_count % 100) == 0)
   serial_debug.print("dropped packet count: ");
   serial_debug.println(dropped_packet_count, DEC);
   serial_debug.print("timeout count: ");
-  serial_debug.println(watchdog_count, DEC);
+  serial_debug.println(watchdog_count);
+  
+  uint8_t databytes[sizeof(update_crc_data_packet)];
+  memcpy((void *)&databytes, (void *)&update_crc_data_packet, sizeof(update_crc_data_packet));
+  for (debug_index = 0; debug_index < sizeof(update_crc_data_packet); debug_index++)
+  {
+    serial_debug.print(" ");
+    serial_debug.print(databytes[debug_index], HEX);
+  }
+  serial_debug.println("  done with bytes");
 }
 #endif
   if (watchdog_exceeded == true)
   {
     watchdog_count++;    
-    comm.public_command_packet.alarm_bits |= 1 << ALARM_DROPPED_PACKET;
     watchdog_exceeded = false;
     clear_input = true;
   }
   else
   {
-    if ((comm.public_command_packet.sequence_count != last_sequence_count))
+    if ((command_packet_from_serial.sequence_count != last_sequence_count))
     {
       dropped_packet_count++;
 #ifdef SERIAL_DEBUG
       serial_debug.print("unexpected sequence count: ");
-      serial_debug.println(comm.public_command_packet.sequence_count, DEC);
+      serial_debug.println(command_packet_from_serial.sequence_count, DEC);
 #endif    
-      comm.public_command_packet.alarm_bits |= 1 << ALARM_CRC_ERROR;
       clear_input = true;
     }
     else
     {
-      calc_crc = CRC16.ccitt((uint8_t *)&comm.public_command_packet, sizeof(comm.public_command_packet) - 2);
+      calc_crc = CRC16.ccitt((uint8_t *)&command_packet_from_serial, sizeof(command_packet_from_serial) - 2);
 #ifdef SERIAL_DEBUG
       serial_debug.print("CRC calculated from command packet: ");
       serial_debug.println(calc_crc, DEC);
 #endif      
-      if (comm.public_command_packet.crc != calc_crc)
+      if (command_packet_from_serial.crc != calc_crc)
       {
         dropped_packet_count++;
-        comm.public_command_packet.alarm_bits |= 1 << ALARM_CRC_ERROR;
 #ifdef SERIAL_DEBUG
         serial_debug.print("bad CRC 0x ");
-        serial_debug.println(comm.public_command_packet.crc, HEX);
+        serial_debug.println(command_packet_from_serial.crc, HEX);
 #endif  
         clear_input = true;
       }
       else
       {
-          comm.public_command_packet.alarm_bits &= ~(1 << ALARM_CRC_ERROR); 
+        memcpy((void *)&comm.public_command_packet, (void *)&command_packet_from_serial, sizeof(comm.public_command_packet));
+#ifdef SERIAL_DEBUG
+        serial_debug.print("Successful packet received CRC from command packet: ");
+        serial_debug.println(comm.public_command_packet.crc, DEC);
+#endif           
       }          
     }
   }  
-#ifdef SERIAL_DEBUG
-  serial_debug.print("CRC from command packet: ");
-  serial_debug.println(comm.public_command_packet.crc, DEC);
-#endif 
+
   PT_RESTART(pt);
   PT_END(pt);
 }
