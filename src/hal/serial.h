6f21d1e5976527265fef52ded6985ced54e0c242
Mcinnis <russ.mcinnis@intel.com>
2020-04-12 10:05:41 -0700

    updated serial.h to use #define for serial ports and other settings (interval, watchdog, baud)

--- src/hal/serial.h
+++ src/hal/serial.h
@@ -1,10 +1,14 @@
 
 #ifndef __SERIAL_HAL_H__
 #define __SERIAL_HAL_H__
-
-//#define SERIAL_DEBUG
-
 #include "../hal/hal.h"
+
+#define BAUD_RATE 38400
+#define WATCHDOG_MS 200  // maximum time to receive all bytes from command packet. if time expires trash data and send next packet.
+#define SEND_INTERVAL_MS 100 // minimum time between sending packets. The time could take longer if response is late.
+
+#define SERIAL_UI Serial1
+//#define SERIAL_DEBUG Serial2
 
 int serialHalInit(void);
 int serialHalGetData(void);
