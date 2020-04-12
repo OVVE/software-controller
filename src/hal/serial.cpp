f240f10751e339bc435e69bd3a23fcf83784b698
Mcinnis <russ.mcinnis@intel.com>
2020-04-12 10:04:31 -0700

    updated serial.cpp to use macros for Serial, Serial1 also macros for settings (BAUD, interval, watchdog) these are defined in serial.h

--- src/hal/serial.cpp
+++ src/hal/serial.cpp
@@ -5,22 +5,12 @@
 #include "../hal/serial.h"
 #include "../modules/link.h"
 
-#define BAUD_RATE 38400
-
-#ifdef SERIAL_DEBUG
-HardwareSerial &serial_debug = Serial;
-#endif
-
-HardwareSerial &serial_ui = Serial1;
-
 uint32_t last_send_ms = 0;        // this is used for send interval
 uint32_t current_send_ms = 0;     // current time is referenced a few times so refer to this variable 
-uint32_t send_interval_ms = 100; //100;  // delay between sending data packets. Could extend by 50msec watchdog timeout though, just do not send faster than this pace
 uint16_t bytesSent;               // Serial.write() returns this - we should increase the default Serial buffer size so that the function does not block
 uint8_t inByte;                   // store each byte read
 int incoming_index = 0;           // index for array of bytes received as we are getting them one at a time
 
-uint32_t watchdog_max_ms = 200; //90; //70; //50;    // watch dog time out. After sending data packet wait this long for a command/confirm packet. If timeout then send next data packet.
 uint32_t watchdog_start_ms;       // save the watchdog start time
 bool read_active = false;     // watchdog timer in progress (waiting for command/confirm packet)
 bool watchdog_exceeded = false;   // flag for watchdog timer received. this is for link.cpp.
@@ -43,15 +33,15 @@ extern command_packet_def command_packet_from_serial;
 
 int serialHalInit(void)
 {
-  serial_ui.begin(BAUD_RATE);  // this function does not return anything
+  SERIAL_UI.begin(BAUD_RATE);  // this function does not return anything
   // not sure how we can do something like while (!serial)
   // return MODULE_FAIL;
   do
-  { serial_ui.read();
-  } while (serial_ui.available() > 0);
+  { SERIAL_UI.read();
+  } while (SERIAL_UI.available() > 0);
   
 #ifdef SERIAL_DEBUG
-  serial_debug.begin(BAUD_RATE);
+  SERIAL_DEBUG.begin(BAUD_RATE);
 #endif  
   return HAL_OK;
 }
@@ -63,9 +53,9 @@ int serialHalGetData(void)
     return HAL_IN_PROGRESS;
   }
   // change to while if reading all the bytes currently available
-  if (serial_ui.available())
+  if (SERIAL_UI.available())
   {       
-    serial_ui.readBytes(&inByte, 1);
+    SERIAL_UI.readBytes(&inByte, 1);
     
     command_packet_u.command_bytes[incoming_index] = inByte;
     
@@ -80,24 +70,24 @@ int serialHalGetData(void)
 
       memcpy((void *)&command_packet_from_serial, (void *)&command_packet_u.command_packet, sizeof(command_packet_from_serial));
 #ifdef SERIAL_DEBUG
-      serial_debug.print("sequence from Rpi: ");
-      serial_debug.print(command_packet_from_serial.sequence_count, DEC);
-      serial_debug.print(" ");
-      serial_debug.println(command_packet_from_serial.crc, DEC);
-      serial_debug.println(" ");
+      SERIAL_DEBUG.print("sequence from Rpi: ");
+      SERIAL_DEBUG.print(command_packet_from_serial.sequence_count, DEC);
+      SERIAL_DEBUG.print(" ");
+      SERIAL_DEBUG.println(command_packet_from_serial.crc, DEC);
+      SERIAL_DEBUG.println(" ");
 #endif      
       return HAL_OK;
     } 
   }
   if (read_active == true)
   {
-    if ((millis() - watchdog_start_ms) > watchdog_max_ms)
+    if ((millis() - watchdog_start_ms) > WATCHDOG_MS)
     {
 #ifdef SERIAL_DEBUG
-      serial_debug.print("!!!Watchdog timeout, bytes received count: ");
-      serial_debug.println(incoming_index, DEC);
-      serial_debug.print("first byte (sequence count): ");
-      serial_debug.println(command_packet_u.command_packet.sequence_count, DEC);
+      SERIAL_DEBUG.print("!!!Watchdog timeout, bytes received count: ");
+      SERIAL_DEBUG.println(incoming_index, DEC);
+      SERIAL_DEBUG.print("first byte (sequence count): ");
+      SERIAL_DEBUG.println(command_packet_u.command_packet.sequence_count, DEC);
 #endif        
       read_active = false; 
       watchdog_exceeded = true; 
@@ -111,24 +101,24 @@ int serialHalGetData(void)
 int serialHalSendData()
 {
   current_send_ms = millis();
-  if ((current_send_ms - last_send_ms) >= send_interval_ms && read_active != true)
+  if ((current_send_ms - last_send_ms) >= SEND_INTERVAL_MS && read_active != true)
   {
     if (clear_input == true)
     {
       clear_input = false;
       do
-      { serial_ui.read();
-      } while (serial_ui.available() > 0);
+      { SERIAL_UI.read();
+      } while (SERIAL_UI.available() > 0);
     }
 #ifdef SERIAL_DEBUG
-    serial_debug.print("Sent to Rpi sequence: ");
-    serial_debug.print(update_crc_data_packet.sequence_count, DEC);
-    serial_debug.print(" CRC: ");
-    serial_debug.print(update_crc_data_packet.crc, DEC);
-    serial_debug.println(" ");
+    SERIAL_DEBUG.print("Sent to Rpi sequence: ");
+    SERIAL_DEBUG.print(update_crc_data_packet.sequence_count, DEC);
+    SERIAL_DEBUG.print(" CRC: ");
+    SERIAL_DEBUG.print(update_crc_data_packet.crc, DEC);
+    SERIAL_DEBUG.println(" ");
 #endif 
        
-    bytesSent = serial_ui.write((byte *)&update_crc_data_packet, sizeof(update_crc_data_packet));
+    bytesSent = SERIAL_UI.write((byte *)&update_crc_data_packet, sizeof(update_crc_data_packet));
     
     if (bytesSent != sizeof(update_crc_data_packet)) {
       // handle error
@@ -140,8 +130,8 @@ int serialHalSendData()
     watchdog_start_ms = millis();
     incoming_index = 0; // set this for incoming bytes
 #ifdef SERIAL_DEBUG
-    serial_debug.print("watchdog_start_ms initialized: ");
-    serial_debug.println(watchdog_start_ms, DEC);
+    SERIAL_DEBUG.print("watchdog_start_ms initialized: ");
+    SERIAL_DEBUG.println(watchdog_start_ms, DEC);
 #endif    
     read_active = true;
     return HAL_OK;
