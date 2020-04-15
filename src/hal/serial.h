d504bec19f7be91fa4d3796b64e087b3616aef95
Mcinnis <russ.mcinnis@intel.com>
2020-04-15 09:08:43 -0700

    updated the watchdog timeout for arduino to 200. It was left at 1000msec by accident.

--- src/hal/serial.h
+++ src/hal/serial.h
@@ -6,7 +6,7 @@
 #include "../hal/hal.h"
 
 #define BAUD_RATE 38400
-#define WATCHDOG_MS 1000  // maximum time to receive all bytes from command packet. if time expires trash data and send next packet.
+#define WATCHDOG_MS 200  // maximum time to receive all bytes from command packet. if time expires trash data and send next packet.
 #define SEND_INTERVAL_MS 100 // minimum time between sending packets. The time could take longer if response is late.
 #define SEND_MAX_TIME_MS 30  // maximum time to allow for sending data packet. This is used with sending data in parts if send buffer full.
 #define USE_AVAILABLE_WRITE  // #ifdef in serial.cpp for checking the available bytes on write queue before sending to keep from blocking
