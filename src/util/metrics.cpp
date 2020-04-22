ef1df5e4dd6c5ddd10c6ddb6b708603c5e86fc34
Rosen, Michael R <michael.r.rosen@intel.com>
2020-04-21 19:18:28 -0700

    metrics: add first rev of metric collection for main loop
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

---
+++ src/util/metrics.cpp
@@ -0,0 +1,44 @@
+
+#include <stdint.h>
+
+// TODO: Clean up Arduino code from this file
+#include <Arduino.h>
+
+#include "../util/utils.h"
+#include "../util/metrics.h"
+
+void metricsReset(struct metrics* metrics)
+{
+  metrics->count = 0;
+  metrics->average = 0;
+  metrics->maximum = 0;
+  metrics->minimum = UINT32_MAX;
+  return;
+}
+
+void metricsStart(struct metrics* metrics)
+{
+  metrics->current = (uint32_t) micros();
+  return;
+}
+
+void metricsStop(struct metrics* metrics)
+{
+  uint32_t time = ((uint32_t) micros()) - metrics->current;
+  metrics->count++;
+  
+  // Handle rollover of sum (which should always rollover faster than sum
+  if (metrics->sum + time < metrics->sum) {
+    // TODO: Find a better solution
+    //       For now, set sum to a reasonable multiplier of the average so the current
+    //       time can continue to be rolled in without dominating the average
+    metrics->count = 100;
+    metrics->sum = metrics->average * metrics->count;
+  }
+  
+  metrics->sum += time;
+  metrics->average = metrics->sum / metrics->count;
+  metrics->minimum = min(time, metrics->minimum);
+  metrics->maximum = max(time, metrics->maximum);
+  return;
+}
