7f582fddf1646282f95ac5df4e8c75211eab33d0
JanS <jan@stumpf.eu>
2020-04-24 19:12:30 +0200

    First implementation of closed loop PID control
    not tested in HW yet

--- src/modules/control.cpp
+++ src/modules/control.cpp
@@ -36,7 +36,7 @@ static int8_t motorCompressionDistance;
 // Velocity max = 20 rpm
 static unsigned int currentPosition = 0;
 static unsigned int targetPosition = 0;
-static unsigned int targetVelocity = 0;
+static float targetAirFlow = 0.0f;
 static unsigned int tolerancePosition = 0;
 static unsigned int trapezoidRatio = 2;             // TEMPORARILY SET
 static uint32_t rampTime = 0;
@@ -44,8 +44,8 @@ static uint32_t platTime = 0;
 static uint32_t elapsedTime = 0;
 static uint32_t totalBreathTime = 0;
 static uint32_t targetInhalationTime = 0;
-static float velocityScale = 0.0;
-static float nomVelocity = 0.0;
+static float velocityScale = 0.0f;
+static float nomVelocity = 0.0f;
 
 
 // Pre-compute the trajectory based on known set points and timing requirements
@@ -61,17 +61,18 @@ static void computeTrajectory()
 
     // Position information needs to be integrated to allow for adaptive positioning on exhalation
     targetPosition = 5;
-    rampTime = (totalBreathTime - targetInhalationTime) / (2 + trapezoidRatio);
+    rampTime = (totalBreathTime - targetInhalationTime) / (2.0f + trapezoidRatio);
     platTime = trapezoidRatio * rampTime;
-    nomVelocity = abs((float)targetPosition - (float)currentPosition) * 1000.0 * 1000000.0 * 60.0 / ((float)(rampTime + platTime) * 360.0);
+    nomVelocity = abs((float)targetPosition - (float)currentPosition) * 1000.0f * 1000000.0f * 60.0f / ((float)(rampTime + platTime) * 360.0f);
+
 
   } else if (control.state == CONTROL_BEGIN_INHALATION) {
 
     // EVENTUALLY REFACTORING FOR VOLUME TRAJECTORY, temporarily position tracking for testing
     targetPosition = 40;
-    rampTime = targetInhalationTime / (2 + trapezoidRatio);
+    rampTime = targetInhalationTime / (2.0f + trapezoidRatio);
     platTime = trapezoidRatio * rampTime;
-    nomVelocity = abs((float)targetPosition - (float)currentPosition) * 1000.0 * 1000000.0 * 60.0 / ((float)(rampTime + platTime) * 360.0);
+    nomVelocity = abs((float)targetPosition - (float)currentPosition) * 1000.0f * 1000000.0f * 60.0f / ((float)(rampTime + platTime) * 360.0f);
 
   } else {
     // Blank
@@ -88,53 +89,116 @@ static void updateTrajectory()
   if (elapsedTime < rampTime) {
     // Ramp up
     velocityScale = ((float)elapsedTime / (float)rampTime);
-    targetVelocity = (unsigned int)(nomVelocity * velocityScale);
 
   } else if ((elapsedTime > rampTime) && (elapsedTime < (platTime + rampTime))) {
     // Plateau
     velocityScale = 1.0;
-    targetVelocity = (unsigned int)(nomVelocity * velocityScale);
 
   } else {
     // Ramp down
     velocityScale = (float)(2 * rampTime + platTime - elapsedTime) / (float)rampTime;
-    targetVelocity = (unsigned int)(nomVelocity * velocityScale);
 
   }
 
-  // Debug Output Section
+ targetAirFlow = (nomVelocity * velocityScale);
+ // Debug Output Section
   DEBUG_PRINT_EVERY(100, "Target Position: %lu ; Nominal Velocity: %lu ; Target Velocity: %lu", 
-    (uint32_t)targetPosition, (uint32_t)nomVelocity, (uint32_t)targetVelocity);
+    (uint32_t)targetPosition, (uint32_t)nomVelocity, (uint32_t)targetAirFlow);
   // DEBUG_PRINT_EVERY(1000, "Elapsed Time: %lu ; Ramp Time: %lu ; Velocity Scale: %lu", elapsedTime, rampTime, (uint32_t)(1000.0 * velocityScale));
   // DEBUG_PRINT_EVERY(1000, "Proportion of Phase: %lu", velocityScale);
 
 }
 
 // Update control commands and handle ISR/control flags
-static int updateControl()
+static int updateControl(void)
 {
 
-  // Update trajectory to get instantaneous target air flow
-  updateTrajectory();
+    float flowSensorInput;
+    static float lastFlowSensorInput=0.0f;
+    float controlP,controlD;
+    static float controlI=0.0f;
+    float controlOut;
+    float controlOutLimited;
 
-  // ADD FUNCTIONALITY FOR ISR FLAG
-  if ((timerHalRun(&controlTimer)) == HAL_TIMEOUT ) {
-    return 1;
-  }
+    //TEMPORARY ASSIGNMENT. SHOULD BE STORED IN PARAMETERS
+    float Kf=0.8f;
+    float Kp=0.1f;
+    float Ki=.0f;
+    float Kd=.0f;
+    float KiMax=10.0f; //max speed adjustment because of I-part in % of nominalVelocity
+    float controlMax=20.0f; //max speed adjustment of current target velocity
 
-  // **************************************************************
-  // Here is where closed loop control structure will be developed
-  // **************************************************************
-  // Depending on whether we are in inhalation or exhalation, we will
-  // switch between closed loop air flow tracking and closed loop position
-  // tracking. The updateControl() function will accept a flag set by an ISR.
+    //Timing
+    long startMicros=0;
+    long timeToComputeControl;
+    static long timeToComputeControlFiltered=0;
+    startMicros=micros();
 
-  // Send motor commands
-  motorHalCommand(targetPosition, targetVelocity);
+    // Update trajectory to get instantaneous target air flow
+    updateTrajectory();
 
-  // Return unfinished
-  return 0;
+    // ADD FUNCTIONALITY FOR ISR FLAG
+    if ((timerHalRun(&controlTimer)) == HAL_TIMEOUT ) {
+      return 1;
+    }
 
+    // **************************************************************
+    // Here is where closed loop control structure will be developed
+    // **************************************************************
+    // Depending on whether we are in inhalation or exhalation, we will
+    // switch between closed loop air flow tracking and closed loop position
+    // tracking. The updateControl() function will accept a flag set by an ISR.
+
+
+    //read sensor inputs
+    flowSensorInput=targetAirFlow;//needs to be getFlowSensorInput
+
+    //calculate proportional part
+    controlP=targetAirFlow-flowSensorInput;
+
+    //calculate differential part
+    controlD=lastFlowSensorInput-flowSensorInput;
+
+    //calculate integral part
+    controlI+=controlP;
+
+    //limit integral part
+    if ((Ki*controlI)>(nomVelocity*KiMax/100.0f))
+        controlI=(nomVelocity*KiMax/100.0f)/Ki;
+
+    if ((Ki*controlI)<(-nomVelocity*KiMax/100.0f))
+        controlI=-(nomVelocity*KiMax/100.0f)/Ki;
+
+
+    //limit and output
+
+    controlOut=Kp*controlP+Kd*controlD+Ki*controlI;
+
+
+    if (controlOut>(controlMax/100.0f*targetAirFlow))
+        controlOut=(controlMax/100.0f*targetAirFlow);
+
+    if (controlOut<-(controlMax/100.0f*targetAirFlow))
+        controlOut=-(controlMax/100.0f*targetAirFlow);
+
+    //final control output is feed forward + limited controlOut
+    controlOutLimited=Kf*targetAirFlow+controlOut;
+
+    // Send motor commands
+    motorHalCommand(targetPosition, controlOutLimited);
+
+    lastFlowSensorInput=flowSensorInput;
+
+    timeToComputeControl=micros()-startMicros;
+
+    if (timeToComputeControl>0) //micros() overruns every 70mins, catch this here
+    {
+        timeToComputeControlFiltered=9*timeToComputeControlFiltered+timeToComputeControl;
+        timeToComputeControlFiltered/=10;
+        DEBUG_PRINT_EVERY(1000,"Control Stats: Avg us: %ul\n",timeToComputeControlFiltered);
+    }
+    // Return unfinished
+    return 0;
 }
 
 static PT_THREAD(controlThreadMain(struct pt* pt))
