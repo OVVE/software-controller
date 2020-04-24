abf6a9afe8b9502ebbd4b85fb4fc0f68f6c029a0
Rosen, Michael R <michael.r.rosen@intel.com>
2020-04-23 17:17:43 -0700

    main: make metrics turn off/on via macro
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,16 @@
 #define DEBUG_MODULE "main"
 #include "util/debug.h"
 
+#define MAIN_METRICS
+
+#ifdef MAIN_METRICS
 static struct metrics mainLoopMetrics;
 static struct metrics scheduleMetrics;
 static struct metrics controlModuleMetrics;
 static struct metrics sensorsModuleMetrics;
 static struct metrics parametersModuleMetrics;
 static struct metrics linkModuleMetrics;
+#endif
 
 void mainSetup(void)
 {
@@ -31,47 +35,68 @@ void mainSetup(void)
   parametersModuleInit();
   linkModuleInit();
 
+#ifdef MAIN_METRICS
   metricsReset(&mainLoopMetrics);
   metricsReset(&scheduleMetrics);
   metricsReset(&controlModuleMetrics);
   metricsReset(&sensorsModuleMetrics);
   metricsReset(&parametersModuleMetrics);
   metricsReset(&linkModuleMetrics);
+#endif
   DEBUG_PRINT("setup done");
 }
 
 void mainLoop(void)
 {
+#ifdef MAIN_METRICS
   metricsStart(&mainLoopMetrics);
   metricsStart(&scheduleMetrics);
-  
+#endif
+
   // Run all modules in RR; take specified actions in the event of failure
+#ifdef MAIN_METRICS
   metricsStart(&controlModuleMetrics);
+#endif
   if (controlModuleRun() != MODULE_OK) {
     // TODO: control module exited, trigger severe error
   }
+#ifdef MAIN_METRICS
   metricsStop(&controlModuleMetrics);
+#endif
   
+#ifdef MAIN_METRICS
   metricsStart(&sensorsModuleMetrics);
+#endif
   if (sensorsModuleRun() != MODULE_OK) {
     // TODO: sensor module exited, trigger severe error  
   }
+#ifdef MAIN_METRICS
   metricsStop(&sensorsModuleMetrics);
+#endif
 
+#ifdef MAIN_METRICS
   metricsStart(&parametersModuleMetrics);
+#endif
   if (parametersModuleRun() != MODULE_OK) {
     // TODO: parameters module exited, attempt recovery
   }
+#ifdef MAIN_METRICS
   metricsStop(&parametersModuleMetrics);
+#endif
 
+#ifdef MAIN_METRICS
   metricsStart(&linkModuleMetrics);
+#endif
   if (linkModuleRun() != MODULE_OK) {
     // TODO: link module exited, attempt recovery
   }
+#ifdef MAIN_METRICS
   metricsStop(&linkModuleMetrics);
+#endif
 
   // TODO: Failure actions (alarm, watchdog)
 
+#ifdef MAIN_METRICS
   metricsStop(&scheduleMetrics);
   DEBUG_PRINT_EVERY(25000, "Runtime Metrics:");
   DEBUG_PRINT_EVERY(25000, " Main loop:  %10lu us : %10lu us : %10lu us",
@@ -99,4 +124,5 @@ void mainLoop(void)
                     linkModuleMetrics.average,
                     linkModuleMetrics.maximum);
   metricsStop(&mainLoopMetrics);
+#endif
 }
