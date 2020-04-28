cd05e258ce107978c456abb2e257c2cc6824dd62
Rosen, Michael R <michael.r.rosen@intel.com>
2020-04-28 11:07:14 -0700

    estop: add estop hal
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

---
+++ src/hal/estop.cpp
@@ -0,0 +1,21 @@
+
+#include <stdbool.h>
+
+#include <Arduino.h>
+
+#include "../hal/hal.h"
+#include "../hal/estop.h"
+
+#define ESTOP_PIN                4
+#define ESTOP_ASSERTION_LEVEL HIGH
+
+int estopHalInit(void)
+{
+  pinMode(ESTOP_PIN, OUTPUT);
+  return HAL_OK;
+}
+
+bool estopHalCheck(void)
+{
+  return (digitalRead(ESTOP_PIN) == ESTOP_ASSERTION_LEVEL);
+}
