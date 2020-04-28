7577a0322fb2b86f4529ac63c3d6f5df58ec5232
Rosen, Michael R <michael.r.rosen@intel.com>
2020-04-28 11:07:14 -0700

    estop: add estop hal
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

---
+++ src/hal/estop.h
@@ -0,0 +1,15 @@
+
+#ifndef __ESTOP_HAL_H__
+#define __ESTOP_HAL_H__
+
+#include <stdbool.h>
+
+#include "../hal/hal.h"
+
+// TODO: Doc
+int estopHalInit(void);
+
+// TODO: Doc
+bool estopHalCheck(void);
+
+#endif /* __ESTOP_HAL_H__ */
