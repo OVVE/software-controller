f89683ebe8e02b2d3a1a922d55ec168471cfbe2d
Rosen, Michael R <michael.r.rosen@intel.com>
2020-04-30 16:41:39 -0700

    sensor: add support for the mpxv7007 pressure sensor
    Signed-off-by: Rosen, Michael R <michael.r.rosen@intel.com>

--- src/hal/sensor/pressure.cpp
+++ src/hal/sensor/pressure.cpp
@@ -7,6 +7,9 @@
 
 #define PRESSURE_SENSOR_PIN 1
 
+//#define PRESSURE_SENSOR_MPXV7025
+#define PRESSURE_SENSOR_MPXV7007
+
 static int16_t reading;
 
 int pressureSensorHalInit(void)
@@ -33,8 +36,19 @@ int pressureSensorHalFetch(void)
 
 int pressureSensorHalGetValue(int16_t* value)
 {
+#if defined(PRESSURE_SENSOR_MPXV7025)
   // For the MPXV7025 pressure sensor: (reading / 1024 - 0.5) / 0.018 = P in kPa
+  // From Calvin, why these exact values and the /4, Im not sure
   int32_t pascals = (((int32_t)reading * 217L) - 110995L)>>2; // convert to Pascals
+#elif defined(PRESSURE_SENSOR_MPXV7007)
+  // For the MPXV7007 pressure sensor: (reading / 1024 - 0.5) / 0.057 = P in kPa
+  //                                   (reading / 1024 - 0.5) / (57 / 1000) * 1000 in Pa
+  //                                   (reading / 1024 - 0.5) * 1000000 / 57
+  //                                   (reading * 1000000 / 57 / 1024 - 500000 / 57)
+  //                                   (reading * 17.1327 - 8771.9298) * 8 / 8  ; Help make multiplier error smaller
+  //                                   (reading * 137.0614 - 70175.4386) / 8
+  int32_t pascals = (((int32_t)reading * 137L) - 70175L)>>3; // convert to Pascals
+#endif
   int32_t u100umH2O = (pascals * 4177L)>>12;                  // convert Pascals to 0.1mmH2O
   *value = (int16_t)u100umH2O;                                // return as 16 bit signed int
   return HAL_OK;
