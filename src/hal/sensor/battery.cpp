d2bb47508c4fc36320488080db5272001d52b11c
zmk5 <zkakish@gmail.com>
2020-05-14 12:03:57 -0700

    initial battery implementation

--- src/hal/sensor/battery.cpp
+++ src/hal/sensor/battery.cpp
@@ -1,9 +1,39 @@
 
+#include <stdint.h>
+
 #include "../../hal/hal.h"
+#include "../../hal/sensor/adc.h"
 #include "../../hal/sensor/battery.h"
+
+#define BATTERY_SENSOR_PIN  3
+#define SENSOR_RESOLUTION   1024
+#define BATTERY_RANGE       5
+#define VOLTAGE_DIVISION    4
+
+static int16_t reading;
 
 int batterySensorHalInit(void)
 {
-  // TODO: Implement
+  adcHalInit();
+
+  return HAL_OK;
+}
+
+int batterySensorHalFetch(void)
+{
+  if (!adcHalBusy()) {
+    adcHalBegin(BATTERY_SENSOR_PIN);
+  } else if ((adcHalGetCurrentPin() == BATTERY_SENSOR_PIN) && !adcHalInProgress()) {
+    reading = adcHalGetValue();
+    adcHalComplete();
+    return HAL_OK;
+  }
+
+  return HAL_IN_PROGRESS;
+}
+
+int batterySensorHalGetValue(float16_t* value)
+{
+  *value = ((float16_t) reading / SENSOR_RESOLUTION) * (BATTERY_RANGE * VOLTAGE_DIVISION)
   return HAL_OK;
 }
--- src/hal/sensor/battery.h
+++ src/hal/sensor/battery.h
@@ -2,11 +2,17 @@
 #ifndef __BATTERY_SENSOR_HAL_H__
 #define __BATTERY_SENSOR_HAL_H__
 
+#include <stdint.h>
+
 #include "../../hal/hal.h"
 
 // TODO: Doc
 int batterySensorHalInit(void);
 
-// TODO: Rest of Battery API
+// TODO: Doc
+int batterySensorHalFetch(void);
+
+// TODO: Doc
+int batterySensorHalGetValue(float16_t* value);
 
 #endif /* __BATTERY_SENSOR_HAL_H__ */
